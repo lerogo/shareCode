87803024b9209f72b56947380e7cf2ef7051267d
lerogo <lerogo@163.com>
2021-05-06 11:05:30 +0800

    修改计组实验代码为压缩包

--- njust_homework/计组/3/simplecpu32/Reggroup32/Reggroup32.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2017.1\\bin
-call %xv_path%/xelab  -wto b8e896dbe727493d80599663c217e8ac -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot reggroup32_tb_behav xil_defaultlib.reggroup32_tb xil_defaultlib.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
--- njust_homework/计组/4/singlemips32cpu/cpu.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2017.1\\bin
-call %xv_path%/xsim SingleCPU_tb_behav -key {Behavioral:sim_1:Functional:SingleCPU_tb} -tclbatch SingleCPU_tb.tcl -view E:/singleMIPSCPU32/cpu/SingleCPU_tb_behav.wcfg -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
